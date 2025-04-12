9cac5c3c605182000e99181bba54e747a4c5b741
csRyanSchreck <schreckryan@ufl.edu>
2025-04-11 20:26:26 -0400

    Updated Empty Function with proper code

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -1,21 +1,41 @@
+using System.Text;
+using System.Net.Http;
+using System.IO;
+using System.Threading.Tasks;
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+// for converting the incoming JSON payload into .NET objects
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace EsepWebhook;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input">The event for the Lambda function handler to process.</param>
-    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
-    /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public async Task<string> FunctionHandler(object input, ILambdaContext context)
     {
-        return input.ToUpper();
+        context.Logger.LogInformation($"FunctionHandler received: {input}");
+        
+        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
+        
+        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
+        
+        string slackUrl = Environment.GetEnvironmentVariable("SLACK_URL");
+        if (string.IsNullOrEmpty(slackUrl))
+        {
+            context.Logger.LogInformation("SLACK_URL env variable is not set.");
+            return "SLACK_URL not set";
+        }
+        
+        var client = new HttpClient();
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, slackUrl)
+        {
+            Content = new StringContent(payload, Encoding.UTF8, "application/json")
+        };
+        
+        HttpResponseMessage response = await client.SendAsync(webRequest);
+        
+        using var reader = new StreamReader(await response.Content.ReadAsStreamAsync());
+        return await reader.ReadToEndAsync();
     }
 }
